89141beabef50667a78c2359f5cfc562e6cf30f9
BugenZhao <i@bugenzhao.com>
2020-05-23 00:11:51 +0800

    First commit

---
+++ Formula/mips32-elf-binutils.rb
@@ -0,0 +1,23 @@
+class Mips32ElfBinutils < Formula
+
+  desc "FSF Binutils for mips32-elf cross development"
+  homepage "https://www.gnu.org/software/binutils/binutils.html"
+  url "https://ftp.gnu.org/gnu/binutils/binutils-2.34.tar.xz"
+  mirror "https://ftpmirror.gnu.org/binutils/binutils-2.34.tar.xz" 
+  sha256 "f00b0e8803dc9bab1e2165bd568528135be734df3fabf8d0161828cd56028952"
+
+  def install
+    args = [
+      "--prefix=#{prefix}",
+      "--disable-werror",
+      "--disable-nls",
+      "--target=mips32-elf"
+    ]
+
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+
+    info.rmtree
+  end
+end
---
+++ Formula/mips32-elf-gcc.rb
@@ -0,0 +1,46 @@
+class Mips32ElfGcc < Formula
+
+  desc "GNU compiler collection for mips32-elf development"
+  homepage "https://gcc.gnu.org"
+  url "https://ftp.gnu.org/gnu/gcc/gcc-10.1.0/gcc-10.1.0.tar.xz"
+  mirror "https://mirror.clarkson.edu/gnu/gcc/gcc-10.1.0/gcc-10.1.0.tar.xz" 
+  sha256 "b6898a23844b656f1b68691c5c012036c2e694ac4b53a8918d4712ad876e7ea2"
+
+  depends_on "gmp"
+  depends_on "libmpc"
+  depends_on "mpfr"
+  depends_on "mips32-elf-binutils"
+
+  def install
+    languages = %w[c go]
+
+    binutils = Formulary.factory "mips32-elf-binutils"
+
+    args = [
+      "--prefix=#{prefix}",
+      "--enable-languages=#{languages.join(",")}",
+      "--disable-werror",
+      "--disable-nls",
+      "--disable-libssp",
+      "--disable-libmudflap",
+      "--disable-multilib",
+      "--with-as=#{binutils.bin}/mips32-elf-as",
+      "--with-ld=#{binutils.bin}/mips32-elf-ld",
+      "--with-newlib",
+      "--without-headers",
+      "--target=mips32-elf"
+    ]
+
+    mkdir "build" do
+      system "../configure", *args
+      system "make", "all-gcc"
+      system "make", "install-gcc"
+      system "make", "all-target-libgcc"
+      system "make", "install-target-libgcc"
+    end
+
+    # info and man7 files conflict with native gcc
+    info.rmtree
+    man7.rmtree
+  end
+end
